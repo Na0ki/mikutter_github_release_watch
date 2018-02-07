fa02a464cce6d74d5a42152568815c442c3d4194
ahiru <wellingtonnmaeda0524@gmail.com>
2018-02-07 21:58:09 +0900

    Enumeratorに書き換え

--- github_release_watch.rb
+++ github_release_watch.rb
@@ -9,6 +9,7 @@ require 'uri'
 require_relative 'model'
 
 Plugin.create(:github_release_watch) do
+  GRW = Plugin::GithubReleaseWatch
 
   # 絵文字コード生成
   # 指定された絵文字のフォーマットをする
@@ -19,7 +20,7 @@ Plugin.create(:github_release_watch) do
     code
   end
 
-  @timeout = 86_400
+  @timeout = UserConfig[:github_release_watch_timeout] || 86_400
   @channel = UserConfig[:github_release_watch_slack_channel]
   @username = UserConfig[:github_release_watch_slack_username]
   @webhook_url = UserConfig[:github_release_watch_webhook_url]
@@ -56,16 +57,18 @@ Plugin.create(:github_release_watch) do
   # GitHubのrelease.atomをパースして最新のバージョンを確認する
   def monitor_release
     Thread.new {
-      doc = open(@request_url).read
-      xml = REXML::Document.new(doc)
-      entries = []
-      xml.elements.each('feed/entry') do |entry|
-        entries << Plugin::GithubReleaseWatch::Entry.new(title: entry.elements['title'].text,
-                                                         updated: Time.parse(entry.elements['updated'].text),
-                                                         author: entry.elements['author/name'].text,
-                                                         link: entry.elements['link'].attribute('href'))
-      end
-      entry = entries.max_by { |e| e.updated.to_i }
+      entry = Enumerator.new { |y|
+        doc = open(@request_url).read
+        xml = REXML::Document.new(doc)
+        xml.elements.each('feed/entry') { |e|
+          y << GRW::Entry.new(title: e.elements['title'].text,
+                              updated: Time.parse(e.elements['updated'].text),
+                              author: e.elements['author/name'].text,
+                              link: e.elements['link'].attribute('href'))
+        }
+      }.max_by { |e|
+        e.updated.to_i
+      }
       updated = entry.updated.to_i
       response = nil
       response = post_slack(entry.title, entry.link) if updated > @last_updated
@@ -106,6 +109,8 @@ Plugin.create(:github_release_watch) do
   ## 設定
   settings('GitHubリリース監視') do
     input('監視URL', :github_release_watch_monitor_url)
+    adjustment('取得間隔（秒）', :github_release_watch_timeout, 500, 86_400)
+
     settings('Slack連携') do
       input('webhook_url', :github_release_watch_webhook_url)
       input('投稿チャンネル', :github_release_watch_slack_channel)
